9360a192e710c4df736cbb60b750cbfe2dbf8306
Putuani <niputusumertiani@students.amikom.ac.id>
2023-07-11 08:47:42 +0700

    Add project files.

---
+++ LatihanClassdanObject/Mobil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassdanObject
+{
+    public class Mobil
+    {
+        public string Warna { get; set; }
+        public string Merek { get; set; }
+        public string Model { get; set; }
+        public int TahunKeluaran { get; set; }
+        public int JumlahPintu { get; set; }
+
+        public void input()
+        {
+            Console.WriteLine("Warna : {0} ", this.Warna);
+            Console.WriteLine("Merek : {0} ", this.Merek);
+            Console.WriteLine("Model: {0} ", this.Model);
+            Console.WriteLine("TahunKeluaran: {0} ", this.TahunKeluaran);
+            Console.WriteLine("JumlahPintu : {0} ", this.JumlahPintu);
+
+        }
+
+        public void Klaskson()
+        {
+            Console.WriteLine("Tin..tintin.....tin...tintin....");
+        }
+        public void Info ()
+        {
+            Console.WriteLine("Mobil saya berwarna Putih, merek kijang,model nyt, tahun keluaran 2022, dengan jumlah pinta ada empat");
+        }
+    }
+}
---
+++ LatihanClassdanObject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassdanObject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobil mobil= new Mobil();
+            
+            mobil.Warna = "Putih";
+            mobil.Merek = "Kijang";
+            mobil.Model = "nyt";
+            mobil.TahunKeluaran = 2022;
+            mobil.JumlahPintu = 4;
+
+            mobil.input();
+            mobil.Klaskson();
+            mobil.Info();
+
+            Console.ReadKey();
+
+;
+        }
+    }
+}
